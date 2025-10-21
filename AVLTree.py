348ac5998b0fff3e43e235a3b117b4d0984eb89d
caldeira04 <joao.p.caldeira@icloud.com>
2025-10-21 11:22:02 -0300

    start: verify balance

--- AVLTree.py
+++ AVLTree.py
@@ -1,23 +1,26 @@
 class Node:
-    def __int__(self,value):
+    def __int__(self, value):
         self.value = value
         self.left = None
         self.right = None
         self.height = None
-    
-    def get_height(self,node):
+
+    def get_height(self, node):
         if node is None:
             return 0
         return node.height
+
     def update_height(self):
         left_height = self.get_height(self.left)
         right_height = self.get_height(self.right)
-        self.height = max(left_height,right_height)
-    def get_balance_factor(self,node):
+        self.height = max(left_height, right_height)
+
+    def get_balance_factor(self, node):
         if node is None:
             return 0
         return self.get_height(self.left) - self.get_height(self.right)
-    def rotate_left(self,node):
+
+    def rotate_left(self, node):
         z = node
         y = node.right
         t2 = y.left
@@ -26,7 +29,8 @@ class Node:
         z.update_height(z)
         y.update_height(y)
         return y
-    def rotate_right(self,node):
+
+    def rotate_right(self, node):
         z = node
         y = node.left
         t3 = y.right
@@ -35,6 +39,7 @@ class Node:
         z.update_height()
         y.update_height()
         return y
+
 
 class Arvore:
     def __init__(self):
@@ -81,26 +86,45 @@ class Arvore:
         return False
 
         def insert_avl(self, value):
-        new_node = Node(value)
-        if self.root is None:
-            self.root = new_node
-            return
-        path = []
-        current = self.root
-        while True:
-            path.append(current)
-            if new_node.value < current.value:
-                if current.left is None:
-                    current.left = new_node
-                    break
-                else: 
-                    current = current.left
-            elif new_node.value > current.value:
-                if current.right is None:
-                    current.right = new_node
-                    break
-                else:
-                    current = current.right
-            else:
+            new_node = Node(value)
+            if self.root is None:
+                self.root = new_node
                 return
-            print(f"Nó {value} inserido")
+            path = []
+            current = self.root
+            while True:
+                path.append(current)
+                if new_node.value < current.value:
+                    if current.left is None:
+                        current.left = new_node
+                        break
+                    else:
+                        current = current.left
+                elif new_node.value > current.value:
+                    if current.right is None:
+                        current.right = new_node
+                        break
+                    else:
+                        current = current.right
+                else:
+                    return
+                print(f"Nó {value} inserido")
+
+            reversed = path[::-1]
+
+            for item, index in reversed:  # index para encontrar o nó ancestral
+                updated_height = item.update_height(item)
+                balance_factor = item.get_balance_factor(item)
+
+                if balance_factor > 1:
+                    if new_node.value < item.left:
+                        item.rotate_left(item)
+                    else:
+                        item.rotate_left(item.left)
+                        item.rotate_right(item)
+                if balance_factor < -1:
+                    if new_node.value > item.right:
+                        item.rotate_left(item)
+                    else:
+                        item.rotate_right(item.right)
+                        item.rotate_left(item)
